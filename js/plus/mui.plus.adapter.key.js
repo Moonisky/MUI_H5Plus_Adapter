42b0ab0881f903acd3faab0ec0d86142928d98c3
Thermod <semper_idem@126.com>
2020-03-01 15:34:06 +0800

    修复 plus 被替换的问题

--- js/plus/mui.plus.adapter.key.js
+++ js/plus/mui.plus.adapter.key.js
@@ -15,92 +15,91 @@
   // 模块函数的具体定义参照了 http://www.html5plus.org/doc/h5p.html
   // 由于 Web 和 App 的差异，决定了很多模块无法实现，只能够忽略方法实际效果或者返回模拟值
   // 因此，这些无法正常模拟的 API 会输出警告，以便更好地提醒开发者
-  window.plus = {
+
+  /**
+   * Key管理设备按键事件
+   */
+  window.plus.key = {
     /**
-     * Key管理设备按键事件
+     * 添加按键事件监听器
+     * @param {String} keyevent 要监听的按键事件类型
+     * @param {Function} listener 监听按键事件发生时调用的回调函数
+     * @param {Boolean} capture 捕获按键事件流顺序
      */
-    key: {
-      /**
-       * 添加按键事件监听器
-       * @param {String} keyevent 要监听的按键事件类型
-       * @param {Function} listener 监听按键事件发生时调用的回调函数
-       * @param {Boolean} capture 捕获按键事件流顺序
-       */
-      addEventListener: function(keyevent, listener, capture) {
-        window.addEventListener(
-          keyevent,
-          function(event) {
-            if (listener) {
-              listener(event);
-            }
-          },
-          capture
-        );
-      },
-      /**
-       * 隐藏软键盘
-       * @description 此实现参考 Uni-App H5 hide-keyboard.js 的实现
-       */
-      hideSoftKeybord: function() {
-        var activeElement = document.activeElement;
-        if (
-          activeElement &&
-          (activeElement.tagName === "TEXTAREA" ||
-            activeElement.tagName === "INPUT")
-        ) {
-          activeElement.blur();
-        }
-      },
-      /**
-       * 设置辅助输入类型
-       * @param {Object} type 辅助输入类型
-       * @description 借助 input 控件的 autocomplete 来模拟
-       */
-      setAssistantType: function(type) {
-        // 设置 input 的 list 属性
-        var activeElement = document.activeElement;
-        if (activeElement && activeElement.tagName === "INPUT") {
-          var autocomplete = "on";
-          switch (type) {
-            case "nick":
-              autocomplete = "nickname";
-              break;
-            case "address":
-              autocomplete = "street-address";
-              break;
-            case "tel":
-              autocomplete = "tel";
-              break;
-            case "email":
-              autocomplete = "email";
-              break;
-            case "company":
-              break;
-            case "tax":
-              break;
-            case "id":
-              break;
-            default:
-              break;
+    addEventListener: function(keyevent, listener, capture) {
+      window.addEventListener(
+        keyevent,
+        function(event) {
+          if (listener) {
+            listener(event);
           }
-          activeElement.setAttribute("autocomplete", autocomplete);
-        }
-      },
-      /**
-       * 显示软键盘
-       * @description 原实现中，iOS 需获取DOM中的input元素并调用其focus方法获取焦点才能主动弹出系统软键盘，而 Web 也是如此实现，因此直接标记为不支持
-       */
-      showSoftKeybord: function() {
-        console.warn("Web 端不支持 plus.key.showSoftKeybord 显示软键盘");
-      },
-      /**
-       * 移除按键事件监听器
-       * @param {Object} event 要移除的事件类型
-       * @param {Object} listener 要移除回调函数对象
-       */
-      removeEventListener: function(event, listener) {
-        window.removeEventListener(event, listener);
+        },
+        capture
+      );
+    },
+    /**
+     * 隐藏软键盘
+     * @description 此实现参考 Uni-App H5 hide-keyboard.js 的实现
+     */
+    hideSoftKeybord: function() {
+      var activeElement = document.activeElement;
+      if (
+        activeElement &&
+        (activeElement.tagName === "TEXTAREA" ||
+          activeElement.tagName === "INPUT")
+      ) {
+        activeElement.blur();
       }
+    },
+    /**
+     * 设置辅助输入类型
+     * @param {Object} type 辅助输入类型
+     * @description 借助 input 控件的 autocomplete 来模拟
+     */
+    setAssistantType: function(type) {
+      // 设置 input 的 list 属性
+      var activeElement = document.activeElement;
+      if (activeElement && activeElement.tagName === "INPUT") {
+        var autocomplete = "on";
+        switch (type) {
+          case "nick":
+            autocomplete = "nickname";
+            break;
+          case "address":
+            autocomplete = "street-address";
+            break;
+          case "tel":
+            autocomplete = "tel";
+            break;
+          case "email":
+            autocomplete = "email";
+            break;
+          case "company":
+            break;
+          case "tax":
+            break;
+          case "id":
+            break;
+          default:
+            break;
+        }
+        activeElement.setAttribute("autocomplete", autocomplete);
+      }
+    },
+    /**
+     * 显示软键盘
+     * @description 原实现中，iOS 需获取DOM中的input元素并调用其focus方法获取焦点才能主动弹出系统软键盘，而 Web 也是如此实现，因此直接标记为不支持
+     */
+    showSoftKeybord: function() {
+      console.warn("Web 端不支持 plus.key.showSoftKeybord 显示软键盘");
+    },
+    /**
+     * 移除按键事件监听器
+     * @param {Object} event 要移除的事件类型
+     * @param {Object} listener 要移除回调函数对象
+     */
+    removeEventListener: function(event, listener) {
+      window.removeEventListener(event, listener);
     }
   };
 })(mui, document, window);
--- js/plus/mui.plus.adapter.storage.js
+++ js/plus/mui.plus.adapter.storage.js
@@ -15,53 +15,52 @@
   // 模块函数的具体定义参照了 http://www.html5plus.org/doc/h5p.html
   // 由于 Web 和 App 的差异，决定了很多模块无法实现，只能够忽略方法实际效果或者返回模拟值
   // 因此，这些无法正常模拟的 API 会输出警告，以便更好地提醒开发者
-  window.plus = {
+  
+  /**
+   * Storage模块管理应用本地数据存储区，用于应用数据的保存和读取。
+   * @description 我们使用 localStorage 来代替，localStorage 用于长久保存整个网站的数据，保存的数据没有过期时间，直到手动去删除。
+   */
+  window.plus.storage = {
     /**
-     * Storage模块管理应用本地数据存储区，用于应用数据的保存和读取。
-     * @description 我们使用 localStorage 来代替，localStorage 用于长久保存整个网站的数据，保存的数据没有过期时间，直到手动去删除。
+     * 获取应用存储区中保存的键值对的个数
      */
-    storage: {
-      /**
-       * 获取应用存储区中保存的键值对的个数
-       */
-      getLength: function() {
-        return localStorage.length;
-      },
-      /**
-       * 通过键(key)检索获取应用存储的值
-       * @param {String} key 存储的键值
-       */
-      getItem: function(key) {
-        return localStorage.getItem(key);
-      },
-      /**
-       * 修改或添加键值(key-value)对数据到应用数据存储中
-       * @param {String} key 存储的键值
-       * @param {String} value 存储的内容
-       */
-      setItem: function(key, value) {
-        localStorage.setItem(key, value);
-      },
-      /**
-       * 通过key值删除键值对存储的数据
-       * @param {String} key 存储的键值
-       */
-      removeItem: function(key) {
-        localStorage.removeItem(key);
-      },
-      /**
-       * 清除应用所有的键值对存储数据
-       */
-      clear: function() {
-        localStorage.clear();
-      },
-      /**
-       * 获取键值对中指定索引值的key值
-       * @param {Number} index 存储键值的索引
-       */
-      key: function(index) {
-        return localStorage.key(index);
-      }
+    getLength: function() {
+      return localStorage.length;
+    },
+    /**
+     * 通过键(key)检索获取应用存储的值
+     * @param {String} key 存储的键值
+     */
+    getItem: function(key) {
+      return localStorage.getItem(key);
+    },
+    /**
+     * 修改或添加键值(key-value)对数据到应用数据存储中
+     * @param {String} key 存储的键值
+     * @param {String} value 存储的内容
+     */
+    setItem: function(key, value) {
+      localStorage.setItem(key, value);
+    },
+    /**
+     * 通过key值删除键值对存储的数据
+     * @param {String} key 存储的键值
+     */
+    removeItem: function(key) {
+      localStorage.removeItem(key);
+    },
+    /**
+     * 清除应用所有的键值对存储数据
+     */
+    clear: function() {
+      localStorage.clear();
+    },
+    /**
+     * 获取键值对中指定索引值的key值
+     * @param {Number} index 存储键值的索引
+     */
+    key: function(index) {
+      return localStorage.key(index);
     }
   };
 })(mui, document, window);
